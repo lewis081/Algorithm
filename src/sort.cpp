6389bbf0561150e0e780fa2d9d2de93bd791265f
Lewis <121012648@qq.com>
2017-09-30 13:51:38 +0800

    添加一个自己对二分法的理解

--- src/sort.cpp
+++ src/sort.cpp
@@ -98,6 +98,30 @@ int findIdxUseBinarySearch_Internet(QVector<int> &array, int left, int right, co
     return left;
 }
 
+//我的新想法(): 抽离出对二分法查找特点的理解，再做判断
+//特点： 二分查找(如果没有找到这个元素)法最终肯定left = right,
+//且这个索引的值和目标值邻近,要么在所有大于目标值的最小值索引处，要么在所有小于目标值的最大索引处
+
+//大于val的最小值的idx
+//满足right < size
+int findIdxUseBinarySearch_MyIdea(QVector<int> &array, int left, int right, const int key)
+{
+    //binary search: 要么在所有大于目标值的最小值索引处，要么在所有小于目标值的最大索引处, left == right
+    while (left < right){
+        int middle = (left + right) / 2;
+        if (array[middle] > key)
+            right = middle - 1;
+        else
+            left  = middle + 1;
+    }
+
+    //judge: 根据二分法最终索引的特点 多加一层判定
+    int idx = left;
+    if (array[left]  < key) idx = left + 1;
+
+    return idx;
+}
+
 void insertSort_BinarySerach(QVector<int> &array)
 {
     const int size = array.size();
@@ -105,8 +129,9 @@ void insertSort_BinarySerach(QVector<int> &array)
     for(int idx = 1; idx < size; idx++){//n - 1
         int key = array[idx];
 
-//        int backMoveStartIdx = findIdxUseBinarySearch_Internet(array, 0, idx, key);
-        int backMoveStartIdx = findIdxUseBinarySearch(array, 0, idx, key);
+//        int backMoveStartIdx = findIdxUseBinarySearch_Internet(array, 0, idx-1, key);
+//        int backMoveStartIdx = findIdxUseBinarySearch(array, 0, idx-1, key);
+        int backMoveStartIdx = findIdxUseBinarySearch_MyIdea(array, 0, idx-1, key);
 
         for(int moveIdx = idx-1; moveIdx >= backMoveStartIdx; moveIdx--){
             array[moveIdx+1] = array[moveIdx];
